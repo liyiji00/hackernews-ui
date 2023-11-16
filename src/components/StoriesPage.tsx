67837257acd6e910a3ed0fe15d47fe641da39037
longxiang.li <lilongxiang2000@gmail.com>
2023-11-16 16:10:04 +0800

    refactor: 复用组件抽离

--- src/components/StoriesPage.tsx
+++ src/components/StoriesPage.tsx
@@ -1,6 +1,5 @@
 import { useStoriesByFun } from '~/hooks'
 import { classNames, getDomainHost } from '~/utils'
-import { TypeItemStory } from '~/type'
 
 export default (props: {
   title: string
@@ -13,21 +12,42 @@ export default (props: {
     props.pageSize
   )
 
-  const emptyList = new Array(pageSize)
-    .fill(0)
-    .map<TypeItemStory>((_, index) => ({
-      id: index,
-      type: 'story',
-      by: '',
-      descendants: 0,
-      kids: [],
-      score: 0,
-      title: '',
-      url: '',
-      time: 0,
-    }))
+  const emptyList = new Array<null>(pageSize).fill(null)
 
-  const SplitSymbol = () => <span className="op50">|</span>
+  const Btn = (props: {
+    onClick: (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+    class?: string
+    children?: string | JSX.Element
+  }) => (
+    <button
+      className={classNames(
+        'rd-2 b-0 px-2 py-1 cursor-pointer bg-gray-100',
+        'active:bg-gray-200',
+        'hover:bg-gray-300',
+        props.class
+      )}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  )
+
+  const SplitSymbol = () => <span className="op50 select-none">|</span>
+
+  const Item = (props: {
+    key: number
+    children?: string | JSX.Element | JSX.Element[]
+  }) => (
+    <li
+      key={props.key}
+      className={classNames(
+        'p-2 hover:bg-gray-300 h-10',
+        props.key % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'
+      )}
+    >
+      {props.children}
+    </li>
+  )
 
   return (
     <div className="mb-4">
@@ -35,24 +55,19 @@ export default (props: {
 
       <ul className="list-none p0 my-2 rd-2 overflow-hidden text-base">
         {(loading ? emptyList : data).map((item, index) => {
+          if (item === null) return <Item key={index} />
+
           const itemUrl = `https://news.ycombinator.com/item?id=${item.id}`
           const url = item.url || itemUrl
           const host = item.url ? getDomainHost(item.url) : null
           const date = new Date(item.time * 1000).toLocaleString()
 
           return (
-            <li
-              key={item.id}
-              className={classNames(
-                'p-2 hover:bg-gray-300',
-                index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'
-              )}
-            >
-              <span className="mr-2 op75">
-                {(index + pageSize * pageNum + 1).toString()}.
-              </span>
-
-              <span>
+            <Item key={item.id}>
+              <div>
+                <span className="mr-2 op75 select-none">
+                  {(index + pageSize * pageNum + 1).toString()}.
+                </span>
                 <a
                   className="font-600"
                   target="_blank"
@@ -62,77 +77,68 @@ export default (props: {
                     dangerouslySetInnerHTML={{ __html: item.title || '' }}
                   />
                 </a>
+              </div>
 
-                <p className="text-xs flex gap-2 m-0">
-                  {host && (
-                    <>
-                      <a
-                        className="color-blue-400"
-                        target="_blank"
-                        href={`https://news.ycombinator.com/from?site=${host}`}
-                      >
-                        {host}
-                      </a>
-                      <SplitSymbol />
-                    </>
-                  )}
+              <p className=" text-xs flex gap-2 m-0">
+                {host && (
+                  <>
+                    <a
+                      className="color-blue-400"
+                      target="_blank"
+                      href={`https://news.ycombinator.com/from?site=${host}`}
+                    >
+                      {host}
+                    </a>
+                    <SplitSymbol />
+                  </>
+                )}
 
-                  <a
-                    className=""
-                    target="_blank"
-                    href={itemUrl}
-                  >
-                    {item.score || 0} points{' '}
-                  </a>
+                <a
+                  className=""
+                  target="_blank"
+                  href={itemUrl}
+                >
+                  {item.score || 0} points{' '}
+                </a>
 
-                  <SplitSymbol />
+                <SplitSymbol />
 
-                  <a
-                    className=""
-                    target="_blank"
-                    href={`https://news.ycombinator.com/user?id=${item.by}`}
-                  >
-                    {item.by}
-                  </a>
-                  <SplitSymbol />
+                <a
+                  className=""
+                  target="_blank"
+                  href={`https://news.ycombinator.com/user?id=${item.by}`}
+                >
+                  {item.by}
+                </a>
+                <SplitSymbol />
 
-                  <a
-                    className=""
-                    target="_blank"
-                    href={itemUrl}
-                  >
-                    {date}
-                  </a>
-                  <SplitSymbol />
+                <a
+                  className=""
+                  target="_blank"
+                  href={itemUrl}
+                >
+                  {date}
+                </a>
+                <SplitSymbol />
 
-                  <a
-                    className=""
-                    target="_blank"
-                    href={itemUrl}
-                  >
-                    {item.descendants || 0} comments
-                  </a>
-                </p>
-              </span>
+                <a
+                  className=""
+                  target="_blank"
+                  href={itemUrl}
+                >
+                  {item.descendants || 0} comments
+                </a>
+              </p>
 
               {/* <DevPre obj={item} /> */}
-            </li>
+            </Item>
           )
         })}
       </ul>
 
       {/* 分页信息 */}
       <div className="flex justify-between gap-1 text-xs">
-        <button
-          className={classNames(
-            'rd-2 b-0 px-2 py-1 cursor-pointer',
-            'hover:bg-gray-400',
-            'active:bg-gray-300'
-          )}
-          onClick={prevPage}
-        >
-          prev
-        </button>
+        <Btn onClick={prevPage}>prev</Btn>
 
         <span className="op75">
           <span className="mx-1">all: {Ids.length}</span>
@@ -144,16 +150,7 @@ export default (props: {
           </span>
         </span>
 
-        <button
-          className={classNames(
-            'rd-2 b-0 px-2 py-1 cursor-pointer',
-            'hover:bg-gray-400',
-            'active:bg-gray-300'
-          )}
-          onClick={nextPage}
-        >
-          next
-        </button>
+        <Btn onClick={nextPage}>next</Btn>
       </div>
     </div>
   )
